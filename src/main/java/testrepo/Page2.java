090c9054458c2577ef1f7dbf29e0f17472bccb79
Bhavnadqa <bhavnadqa@gmail.com>
2021-06-23 14:09:18 +0530

    Add3ed code and TCs for Page 2

---
+++ src/main/java/testrepo/Page2.java
@@ -0,0 +1,14 @@
+package testrepo;
+
+public class Page2 {
+	public void P1() {
+		
+		System.out.println("in Page 2");
+	}
+	
+public void P2() {
+		
+		System.out.println("in Page 2");
+	}
+
+}
---
+++ src/test/java/testcases/page2TCs.java
@@ -0,0 +1,10 @@
+package testcases;
+
+public class page2TCs {
+	
+public void P1() {
+		
+		System.out.println("in Page 2 Test cases");
+	}
+
+}
