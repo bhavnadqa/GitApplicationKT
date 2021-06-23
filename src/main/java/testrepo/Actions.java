dcdf5823925cb8944798cace2aaf3ada85ee65b2
Bhavnadqa <bhavnadqa@gmail.com>
2021-06-23 14:13:49 +0530

    created an interface for all the Actions methods and a class for implementing this interface to provide implementations and using this class object ,we can call the methods.

---
+++ src/main/java/testrepo/Actions.java
@@ -0,0 +1,15 @@
+package testrepo;
+
+public class Actions implements methods{
+
+	public void Q1() {
+		System.out.println("click method");
+		
+	}
+
+	public void Q2() {
+		System.out.println("sendkeys method");
+		
+	}
+
+}
---
+++ src/main/java/testrepo/methods.java
@@ -0,0 +1,7 @@
+package testrepo;
+
+public interface methods {
+	
+	void Q1();
+	void Q2();
+}
